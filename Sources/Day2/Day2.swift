eefb4dff6837c44bcd38a8673b68e9f0e0366e5f
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2024-12-02 08:03:01 -0500

    Solve day 2 part 1

--- Sources/Day2/Day2.swift
+++ Sources/Day2/Day2.swift
@@ -8,6 +8,7 @@
 import Foundation
 import ArgumentParser
 import AdventOfCodeUtilities
+import Algorithms
 
 struct Day2: DayCommand {
     static var configuration: CommandConfiguration {
@@ -21,6 +22,45 @@ struct Day2: DayCommand {
     var puzzleInputPath: String
     
     func run() throws {
-        let lines = try readLines()
+        let reports = try readLines().map(Report.init)
+        
+        printTitle("Part 1", level: .title1)
+        let numberOfSafeReports = part1(reports: reports)
+        print("Safe reports:", numberOfSafeReports, terminator: "\n\n")
+    }
+    
+    private func part1(reports: [Report]) -> Int {
+        reports.count(where: \.isSafe)
+    }
+}
+
+private struct Report {
+    let readings: [Int]
+    
+    var isSafe: Bool {
+        var signums = Set<Int>()
+        
+        for window in readings.windows(ofCount: 2) {
+            let difference = window.last! - window.first!
+            
+            if !(1 ... 3).contains(abs(difference)) {
+                return false
+            }
+            
+            let signum = difference.signum()
+            signums.insert(signum)
+            
+            if signums.count == 2 {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
+
+extension Report {
+    init(rawValue: String) {
+        self.readings = rawValue.components(separatedBy: " ").compactMap(Int.init)
     }
 }
