0a1b79d92a041bd795b9bc20e39d02aa5b9b10c6
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2024-12-02 11:20:01 -0500

    Solve day 2 part 2

--- Sources/Day2/Day2.swift
+++ Sources/Day2/Day2.swift
@@ -27,35 +27,88 @@ struct Day2: DayCommand {
         printTitle("Part 1", level: .title1)
         let numberOfSafeReports = part1(reports: reports)
         print("Safe reports:", numberOfSafeReports, terminator: "\n\n")
+        
+        printTitle("Part 2", level: .title1)
+        let numberOfSafeReportsWithDamping = part2(reports: reports)
+        print("Safe reports with damping:", numberOfSafeReportsWithDamping)
     }
     
     private func part1(reports: [Report]) -> Int {
-        reports.count(where: \.isSafe)
+        reports.count(where: { report in
+            let deltas = report.deltas()
+            return deltas.isSafe
+        })
+    }
+    
+    private func part2(reports: [Report]) -> Int {
+        reports.count(where: { report in
+            let deltas = report.deltas()
+            if deltas.isSafe {
+                return true
+            }
+            
+            return report.readings.indices.contains(where: { index in
+                var newReadings = report.readings
+                newReadings.remove(at: index)
+                
+                let report = Report(readings: newReadings)
+                let deltas = report.deltas()
+                return deltas.isSafe
+            })
+        })
     }
 }
 
-private struct Report {
+private struct Report: Equatable {
     let readings: [Int]
+    
+    func deltas() -> Deltas {
+        Deltas(
+            values: readings.windows(ofCount: 2).map({ window in
+                window.last! - window.first!
+            })
+        )
+    }
+}
+
+private struct Deltas {
+    let values: [Int]
     
     var isSafe: Bool {
         var signums = Set<Int>()
         
-        for window in readings.windows(ofCount: 2) {
-            let difference = window.last! - window.first!
-            
-            if !(1 ... 3).contains(abs(difference)) {
+        return values.allSatisfy({ value in
+            if !(1 ... 3).contains(abs(value)) {
                 return false
             }
             
-            let signum = difference.signum()
+            let signum = value.signum()
             signums.insert(signum)
             
             if signums.count == 2 {
                 return false
             }
+            
+            return true
+        })
+    }
+    
+    func isSafeWithDamping() -> Bool {
+        if isSafe {
+            return true
         }
         
-        return true
+        return values.indexed().contains(where: { (index, delta) in
+            var newValues = values
+            newValues.remove(at: index)
+            
+            if index > values.startIndex, index != values.indices.last {
+                newValues[index - 1] += delta
+            }
+            
+            let newDeltas = Deltas(values: newValues)
+            return newDeltas.isSafe
+        })
     }
 }
 
