8881a8b2f1791cc0ead5222f7853632996a37d0c
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2024-12-12 12:46:13 -0500

    Solve day 12 part 1

--- Sources/Day12/Day12.swift
+++ Sources/Day12/Day12.swift
@@ -9,6 +9,7 @@ import Foundation
 import ArgumentParser
 import AdventOfCodeUtilities
 import Collections
+import Algorithms
 
 struct Day12: DayCommand {
     static var configuration: CommandConfiguration {
@@ -22,6 +23,100 @@ struct Day12: DayCommand {
     var puzzleInputPath: String
     
     func run() throws {
-        let input = try readFile()
+        let grid = Grid2D<Character>(rawValue: try readFile()) { $0 }
+        
+        let clock = ContinuousClock()
+        printTitle("Part 1", level: .title1)
+        let (part1Duration, totalPriceOfFencing) = clock.measure {
+            part1(grid)
+        }
+        print("Total price of fencing all regions on the map:", totalPriceOfFencing)
+        print("Elapsed time:", part1Duration, terminator: "\n\n")
+    }
+    
+    func part1(_ grid: Grid2D<Character>) -> Int {
+        let gardenPlotsByPlant = gardenPlotsByPlant(in: grid)
+        
+        return gardenPlotsByPlant.values.lazy
+            .flatMap({ $0 })
+            .reduce(into: 0) { totalPrice, plot in
+                totalPrice += plot.area() * plot.perimeter()
+            }
+    }
+    
+    private func gardenPlotsByPlant(in grid: Grid2D<Character>) -> [Character: [GardenPlot]] {
+        var visited = Set<Point2D>()
+        var gardenPlotsByPlant = [Character: [GardenPlot]]()
+        
+        for (y, x) in product(grid.frame.rows, grid.frame.columns) {
+            let point = Point2D(x: x, y: y)
+            let plant = grid[point]!
+            
+            if visited.contains(point) {
+                continue
+            }
+            
+            let gardenPlot = gardenPlot(containing: point)
+            gardenPlotsByPlant[plant, default: []].append(gardenPlot)
+        }
+        
+        func gardenPlot(containing point: Point2D) -> GardenPlot {
+            let allowedDirections: [Translation2D] = [
+                .up,
+                .right,
+                .down,
+                .left,
+            ]
+            let plant = grid[point]!
+            
+            var gardenPlot = GardenPlot()
+            var stack: Deque<Point2D> = [point]
+            
+            while let currentPoint = stack.popLast() {
+                gardenPlot.insert(currentPoint)
+                visited.insert(currentPoint)
+                
+                for nextPoint in allowedDirections.map({ currentPoint.applying($0) }) {
+                    if visited.contains(nextPoint) {
+                        continue
+                    }
+                    
+                    guard grid.isPointInside(nextPoint) else {
+                        continue
+                    }
+                    
+                    guard grid[nextPoint] == plant else {
+                        continue
+                    }
+                    
+                    stack.append(nextPoint)
+                }
+            }
+            
+            return gardenPlot
+        }
+        
+        return gardenPlotsByPlant
+    }
+}
+
+private typealias GardenPlot = Set<Point2D>
+
+extension GardenPlot {
+    func area() -> Int {
+        count
+    }
+    
+    func perimeter() -> Int {
+        let edges: [Translation2D] = [.up, .right, .down, .left]
+        
+        let perimeter = reduce(into: 0) { perimeter, point in
+            let sidesNotTouchingOthers = edges.count(where: { translation in
+                let neighbor = point.applying(translation)
+                return !contains(neighbor)
+            })
+            perimeter += sidesNotTouchingOthers
+        }
+        return perimeter
     }
 }
